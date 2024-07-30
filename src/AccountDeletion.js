23861533eb45b42adc155c00820a1e27382e076a
Arun Renniy Dsa <arun.r.dsa@gmail.com>
2024-07-30 14:20:55 +0530

    added form for account deletion request

---
+++ src/AccountDeletion.js
@@ -0,0 +1,147 @@
+import { useState } from "react";
+
+const AccountDeletion = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    reason: "",
+    otp: "",
+    otpSent: false,
+  });
+  const { email, reason, otp, otpSent } = formData;
+  const [apiStatus, setApiStatus] = useState({});
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  const generateOTP = async () => {
+    try {
+      const body = JSON.stringify({
+        email,
+      });
+      console.log(body);
+
+      const requestOptions = {
+        method: "POST",
+        headers,
+        body,
+      };
+
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/send-otp`,
+        requestOptions
+      ).then((response) => response.json());
+      setApiStatus(response);
+      setFormData((prev) => ({ ...prev, otpSent: true }));
+    } catch (e) {
+      console.log(e);
+      setApiStatus({
+        success: false,
+        message: "Some error occurred while sending the OTP",
+      });
+    }
+  };
+
+  const deleteAccount = async () => {
+    try {
+      const body = JSON.stringify({
+        email,
+        reason,
+        otp,
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers,
+        body,
+        redirect: "follow",
+      };
+
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/delete-account`,
+        requestOptions
+      ).then((response) => response.json());
+      setApiStatus(response);
+      setFormData((prev) => ({
+        ...prev,
+        email: "",
+        reason: "",
+        otpSent: false,
+        otp: "",
+      }));
+    } catch (e) {
+      console.log(e);
+      setApiStatus({
+        success: false,
+        message: "Some error occurred while sending the OTP",
+      });
+      setFormData((prev) => ({
+        ...prev,
+        otpSent: false,
+        otp: "",
+      }));
+    }
+  };
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    otpSent ? deleteAccount() : generateOTP();
+  };
+
+  return (
+    <div className="App">
+      <h2>Delete account and all the data stored?</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <p className={apiStatus?.success ? "success" : "error"}>
+            {apiStatus?.message}
+          </p>
+        </div>
+        <div>
+          <label>Email: *</label>
+          <input
+            type="email"
+            name="email"
+            onChange={handleOnChange}
+            value={email}
+          />
+        </div>
+        <div>
+          <label>Reason for deletion: *</label>
+          <input
+            type="text"
+            name="reason"
+            onChange={handleOnChange}
+            value={reason}
+          />
+        </div>
+        {otpSent && (
+          <div>
+            <label>OTP: *</label>
+            <input
+              type="text"
+              name="otp"
+              onChange={handleOnChange}
+              value={otp}
+            />
+          </div>
+        )}
+        <button
+          type="submit"
+          disabled={!email || !reason || (otpSent && !otp?.length >= 5)}
+        >
+          {otpSent ? "Delete Account" : "Send OTP"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AccountDeletion;
--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PrivacyPolicy from "./PrivacyPolicy";
+import AccountDeletion from "./AccountDeletion";
+
+const routeConfig = createBrowserRouter([
+  {
+    path: "/",
+    element: <PrivacyPolicy />,
+  },
+  {
+    path: "/account-deletion-request",
+    element: <AccountDeletion />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>Introduction</h1>
-        <p>
-          Your privacy is important to us. This Privacy Policy outlines how we
-          collect, use, and protect your information.
-        </p>
-      </div>
-      <div>
-        <h1>Information Collection</h1>
-        <p>Personal Information: Name, email address</p>
-        <p>Usage Data: Interaction with the app</p>
-      </div>
-      <div>
-        <h1>Use of Information</h1>
-        <p>Provide and improve our services.</p>
-        <p>Communicate updates and offers.</p>
-      </div>
-      <div>
-        <h1>Data Sharing</h1>
-        <p>We do not sell your data.</p>
-      </div>
-      <div>
-        <h1>Data Security</h1>
-        <p>
-          We implement industry-standard measures to protect your data. However,
-          no method is completely secure.
-        </p>
-      </div>
-      <div>
-        <h1>User Rights</h1>
-        <p>You have the right to request deletion of your data.</p>
-      </div>
-      <div>
-        <h1>Changes to Policy</h1>
-        <p>
-          We may update this policy. Changes will be notified via the app or
-          email.
-        </p>
-      </div>
-    </div>
-  );
+  return <RouterProvider router={routeConfig} />;
 }
 
 export default App;
